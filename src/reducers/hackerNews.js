52e0eec4a2f4a74f0b2138f7c86198062789c32e
Sravya GLS <lagrandhi@paypal.com>
2020-05-03 15:03:10 +0530

    console error fix

--- src/reducers/hackerNews.js
+++ src/reducers/hackerNews.js
@@ -1,11 +1,13 @@
 import { FETCH_HACKER_NEWS,FETCH_NEWS_BY_PAGE } from "../actions/index";
-const initialState = {};
+
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_HACKER_NEWS:
       return { ...state, ...action.payload.data };
     case FETCH_NEWS_BY_PAGE:
       return {...state, ...action.payload.data };
+    default: return {
+      ...state
+    }
   }
-  return state;
 }
